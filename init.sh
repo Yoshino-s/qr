c601959d42ccc5b9fc0434798a6023b9418c0107
Yoshino-s <yoshino.prog@gmail.com>
2018-08-21 19:43:36 +0800

    init3

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ pkg install curl wget git
 pip install requests
 wget https://raw.githubusercontent.com/Yoshino-s/qr/master/sources.list
 wget https://raw.githubusercontent.com/Yoshino-s/qr/master/sop.py 
-cp ~/../etc/apt/sources.list ~/../etc/apt/sources.list 
-cp ./sources.list ~/../etc/apt/sources.list 
+cp ~/../usr/etc/apt/sources.list ~/../usr/etc/apt/sources.list 
+cp ./sources.list ~/../usr/etc/apt/sources.list 
 termux-setup-storage
 mkdir /sdcard/sop/
 ln /sdcard/sop/ sop 
