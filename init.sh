190f4bd5ac965ec74048ddcef8e69bd28557b0f7
Yoshino-s <yoshino.prog@gmail.com>
2018-08-21 19:41:43 +0800

    init2

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 pkg install python python-dev
 pkg install curl wget git 
-wget https://
+pip install requests
+wget https://raw.githubusercontent.com/Yoshino-s/qr/master/sources.list
+wget https://raw.githubusercontent.com/Yoshino-s/qr/master/sop.py 
+cp ~/../etc/apt/sources.list ~/../etc/apt/sources.list 
+cp ./sources.list ~/../etc/apt/sources.list 
+termux-setup-storage
+mkdir /sdcard/sop/
+ln /sdcard/sop/ sop 
+cp ./sop.py ~/sop/sop.py 
+echo "Success"
